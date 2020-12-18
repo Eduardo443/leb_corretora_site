fee70c2969878fb43706ead3defb4bee9a4e1c9c
Eduardo443 <eduardobatista020@gmail.com>
2020-12-17 21:10:02 -0300

    add: Footer component

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import Header from "../Template/Header.jsx"
+import Footer from "../Template/Footer.jsx"
 
-export default () => <Header />
+export default () => {
+    return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+    )
+}
---
+++ src/Components/Template/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import "./Footer.css"
+
+export default () => {
+   return( 
+   <footer>
+        <div className="information">
+            <h1 className="tittle">Informações</h1>
+            <div className="paragraph">
+                <h1 className="subtittle">Telefone</h1>
+                <p>(47) 3338-3138</p>
+                <p>(47) 9 9985-2364</p>
+            </div>
+            <div className="paragraph">
+                <h1 className="subtittle">Endereço</h1>
+                <p>Rua: Doutor Pedro Zimmermann</p>
+                <p>Itoupava Central</p>
+                <p>Blumenau - SC</p>
+            </div>
+        </div>
+
+        <div className="localization">
+            <h1 className="tittle">Localização</h1>
+            <div className="map"></div>
+        </div>
+
+        {/* <p>Desenvolvido por Eduardo Hensel Batista</p> */}
+    </footer>
+    )
+}
