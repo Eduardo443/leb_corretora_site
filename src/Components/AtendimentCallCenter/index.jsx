10bf1765c3d72626bce6e03716a734552bc09aba
Eduardo443 <eduardobatista020@gmail.com>
2020-12-30 12:41:47 +0000

    Imgs

--- src/Components/AtendimentCallCenter/index.jsx
+++ src/Components/AtendimentCallCenter/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 
-import CallCenterCard from '../CallCenterCard'
-import { Container, Tittle } from './style'
+import CallCenterCard from '../CallCenterCard/index'
+import { Container, Tittle, Cards } from './style'
+
+import Liberty from '../../assets/imgs/liberty.png'
+import HDI from '../../assets/imgs/hdi.png'
+import Allianz from '../../assets/imgs/allianz.png'
+import Sulamerica from '../../assets/imgs/sulamerica.png'
+import Porto from '../../assets/imgs/porto.png'
+import Azul from '../../assets/imgs/azul.png'
+import Bradesco from '../../assets/imgs/bradesco.png'
+import Sompo from '../../assets/imgs/sompo.png'
+import Tokio from '../../assets/imgs/tokio.png'
+import Zurich from '../../assets/imgs/zurich.png'
 
 export default () => {
     return (
         <Container>
-            <Tittle><h1>Atendimento via 0800</h1></Tittle>
-            <CallCenterCard Name='Liberty' Img={} PhoneNumber='0800 701 4120'/>
+            <Tittle><h1>Atendimento Auto</h1></Tittle>
+            <Cards>
+                <CallCenterCard Img={Liberty} Name='Liberty/Aliro' PhoneNumber='0800 701 4120'/>
+                <CallCenterCard Img={HDI} Name='HDI' PhoneNumber='0800 434 4340'/>
+                <CallCenterCard Img={Allianz} Name='Allianz' PhoneNumber='0800 0130 700'/>
+                <CallCenterCard Img={Sulamerica} Name='Sulamerica' PhoneNumber='0800 727 4100'/>
+                <CallCenterCard Img={Porto} Name='Porto' PhoneNumber='0800 727 2755'/>
+                <CallCenterCard Img={Azul} Name='Azul' PhoneNumber='0800 703 0203'/>
+                <CallCenterCard Img={Bradesco} Name='Bradesco' PhoneNumber='0800 701 2757'/>
+                <CallCenterCard Img={Sompo} Name='Sompo' PhoneNumber='0800 7719 119'/>
+                <CallCenterCard Img={Tokio} Name='Tokio' PhoneNumber='0300 338 6546'/>
+                <CallCenterCard Img={Zurich} Name='Zurich' PhoneNumber='0800 729 1400'/>
+            </Cards>
         </Container>
     );
 }
--- src/Components/AtendimentCallCenter/style.jsx
+++ src/Components/AtendimentCallCenter/style.jsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    height: 95vh;
+    height: 85vh;
     width: 100%;
 
-    display: grid;
-    template-grid: row;
-
     background-color: #E9EDF3 ;
+
+    padding: 5rem;
 `;
 
 export const Tittle = styled.div`
@@ -19,4 +18,12 @@ export const Tittle = styled.div`
     h1{
         margin: 1.5rem;
     }
+`;
+
+export const Cards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+
+    row-gap: 1.8rem;
+    column-gap: 1.8rem;
 `;
