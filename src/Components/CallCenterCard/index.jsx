31ac91a744ac91935df037ce77c6e66af2d36ece
Eduardo443 <eduardobatista020@gmail.com>
2021-01-06 10:23:41 -0300

    add: layout fo CallCenterCard

--- src/Components/CallCenterCard/index.jsx
+++ src/Components/CallCenterCard/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import { Container, Ilustration, Tittle, Number, Text } from './style'
+import { Container, Ilustration, Tittle, Number, Paragraph } from './style'
 
 function CallCenterCard(props){
     return (
         <Container>
             <Ilustration>
-                <a href={props.Img}></a>
+                <img href={props.Img}></img>
             </Ilustration>
-            <Tittle><h1>{props.Name}</h1></Tittle>
-            <Number><h2>{props.PhoneNumber}</h2></Number>
+            <Paragraph>
+                <Tittle><h1>{props.Name}</h1></Tittle>
+                <Number><h2>{props.PhoneNumber}</h2></Number>
+            </Paragraph>
         </Container>
     );
 }
--- src/Components/CallCenterCard/style.jsx
+++ src/Components/CallCenterCard/style.jsx
@@ -1,27 +1,53 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    height: 15rem;
-    width: 10rem;
+    height: 25rem;
+    width: 20rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    border-radius: 10px;
 
     background-color: white;
 `;
 
 export const Ilustration = styled.div`
-    height: 7.5rem;
-    width: 10rem;
+   img{
+       height: 21vh;
+       width: 100%;
+   }
 `;
-
+    
 export const Tittle = styled.div`
-    diplay: flex;
-    align-items: center;
+    display: flex;
+    justify-content: center;
 
+    margin: 15px;
+    
     color: black;
 `;
-
+    
 export const Number = styled.div`
-    diplay: flex;
+    display: flex;
     align-items: center;
+    justify-content: center;
+    
+    height: 50px;
+    width: 100%;
+    border-radius: 0 0 10px 10px;
 
-    color: black;
+    background-color: #5581d9;
+    color: white;
+
+    cursor: pointer;
+
+    :hover{
+        background-color: #316bdf;
+    }
+`;
+
+export const Paragraph = styled.div`
+    display; flex;
 `;
